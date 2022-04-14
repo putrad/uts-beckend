8c56f7fcb06518b6df0f81f71762f08939ed9995
putrad <86851865+putrad@users.noreply.github.com>
2022-04-14 21:23:04 +0700

    Add files via upload

--- Listfilm.js
+++ Listfilm.js
@@ -1,52 +1,14 @@
-let Listfilm =[
-    {id:113,judul:'iron man', genre:'action'},
-    {id:101,judul:'deadpool2', genre:'comedy'},
-    {id:111,judul:'avengers', genre:'fantasy'},
-]
-const findlistfilm = (id) =>{
-    index = 0
-    ketemu = -1
-    while(ketemu== -1){
-        if (id == Listfilm[index].id){
-            ketemu = index
-        }
-        index++
-    }
-    return ketemu
-}
-module.exports ={
-    index : function (request,response){
-        response.json(users)
-    },
-    input : function(request,response){
-        console.log(request.body)
-        response.send(request.body)
-    },
-    getListfilmbyid : function (request,response){
-        id = request.params.id;
-    idx = findlistfilm(id);
-    dataLisfilm = users [idx];
-    response.json(dataLisfilm);
-    },
-    update : function (request, response){
-        const id = request.params.id
-        users.filter(user=>{
-            if(Listfilm.ide == id){
-                Listfilm.id = id
-                user.judul = request.body.judul
-                user.email = request.body.genre
+const express = require ('express');
+const router= express.Router()
+const Listfilmcontroller = require('../controrellers/Listfilm')
 
-                return Listfilm
-            }
-        })
-        response.send(Listfilm)
-    },
-    delete : function(request, response){
-        let id =request.params.id
-        Listfilm = Listfilm.filter(Listfilm => Listfilm.id !=id)
-        response.send(Listfilm)
-    }
-    
-}
+router.route('/Listfilm')
+.get(Listfilmcontroller.index)
+.post(Listfilmcontroller.input)
 
- 
+router.route('/listfilm/:id')
+.get(Listfilmcontroller.getListfilmbyid)
+.update(Listfilmcontroller.update)
+.delete(Listfilmcontroller.delete)
+
+module.exports = router
